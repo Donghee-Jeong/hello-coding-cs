c4cf8b6241c97e6da971b76aa43815b19d2423d9
Donghee-Jeong <weg9511@gmail.com>
2023-12-03 23:31:27 +0900

    제어문 연습

--- Assets/HelloCode.cs
+++ Assets/HelloCode.cs
@@ -5,18 +5,48 @@ public class HelloCode : MonoBehaviour
 {
     private void Start()
     {
-        float distance = GetDistance(2, 2, 5, 6);
-        Debug.Log("(2,2)에서 (5,6)까지의 거리 : " + distance);
-    }
+        // 조건문
+        int love = 80;
 
-    private float GetDistance(int x1, int y1, int x2, int y2)
-    {
-        float width = x2 - x1;
-        float height = y2 - y1;
+        if (love > 90)
+        {
+            Debug.Log("트루엔딩: 히로인과 결혼했다!");
+        } 
+        else if (love > 70)
+        {
+            Debug.Log("굿엔딩: 히로인과 사귀게 되었다!");
+        }
+        else
+        {
+            Debug.Log("배드엔딩: 히로인에게 차였다.");
+        }
 
-        float distance = width * width + height + height;
-        distance = Mathf.Sqrt(distance);
+        // 논리 연산자
+        int age = 11;
 
-        return distance;
+        if (age > 7 && age < 18)
+        {
+            Debug.Log("의무 교육을 받고 있습니다.");
+        }
+
+        if (age < 13 || age > 70)
+        {
+            Debug.Log("일을 할 수 없는 나이입니다.");
+        }
+        
+        // for 문
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(i + "번째 순번입니다.");
+        }
+        
+        // while 문
+        int j = 0;
+
+        while (j < 10)
+        {
+            Debug.Log(j + "번째 루프입니다.");
+            j++;
+        }
     }
 }
