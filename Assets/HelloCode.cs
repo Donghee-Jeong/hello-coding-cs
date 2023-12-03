71f2b4c9fb83662f2d14f9fbb7ebd3bd3c1df97f
Donghee-Jeong <weg9511@gmail.com>
2023-12-03 23:10:08 +0900

    Log 'Hello World'

---
+++ Assets/HelloCode.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class HelloCode : MonoBehaviour
+{
+    private void Start()
+    {
+        // Hello World!를 콘솔에 출력
+        Debug.Log("Hello World!");
+    }
+}
